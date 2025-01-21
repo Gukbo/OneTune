3bf10007de419c0e4d0f71ca371b7b3cc8528f89
Gukbo <2onesound@naver.com>
2025-01-21 14:58:37 +0900

    footer + login (임시)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,19 @@
 import styles from "./footer.module.css";
 import { useState } from "react";
+import githubIcon from "../assets/github.svg";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <div className={styles.footerDiv}>
-      <h1 className={styles.footerH1}>footer임다</h1>
+      <span className={styles.footerSpan}>
+        Copyright 2025. Gukbo all rights reserved
+      </span>
+      <div>
+        <Link to="https://github.com/Gukbo" target="_blank">
+          <img src={githubIcon} alt="githubicon" />
+        </Link>
+      </div>
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,36 @@
 import styles from "../components/login.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate();
+
+  const navigatePlaylist = () => {
+    navigate("/Playlist");
+  };
   return (
     <div className={styles.loginDiv}>
       <div className={styles.loginForm}>
-        <h1 className={styles.loginH1}>로그인 페이지</h1>
+        <h1 className={styles.loginH1}>LOGIN</h1>
+        <div className={styles.IdForm}>
+          <span className={styles.loginSpan}>ID </span>
+          <input
+            className={styles.loginInput}
+            type="text"
+            value="아이디를 입력하세요"
+          />
+        </div>
+        <div className={styles.PwForm}>
+          <span className={styles.loginSpan}>PW</span>
+          <input
+            className={styles.loginInput}
+            type="text"
+            value="비밀번호를 입력하세요"
+            maxLength={24}
+          />
+        </div>
+        <div>
+          <button onClick={navigatePlaylist}>로그인</button>
+        </div>
       </div>
     </div>
   );
